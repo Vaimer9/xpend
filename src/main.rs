fa74a4468c4e9258be4fcbb123d36f34f63cee8f
Vaimer9 <vaim3r9@gmail.com>
2021-10-31 16:46:05 +0530

    Added Some Ideas about the structure of the application

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,31 @@ mod args;
 use args::Args;
 use structopt::StructOpt;
 
+struct User {
+    init: bool
+    money: usize
+    logs: String
+}
+
+impl User {
+    fn new() -> Self {
+        User {
+            init: false,
+            money: 0,
+            logs: String::new()
+        }
+    }
+
+    fn create_logs(mut self) -> Result<(), io::Error> {
+        init::make_folder()?;
+        init::make_file()?;
+        self.init = true;
+    }
+
+    fn log_register() {}
+
+}
+
 fn main() {
     let args = Args::from_args();
 }
